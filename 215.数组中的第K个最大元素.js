a0f7bb160c74e8cec235d02658a78896df1a4b61
Zeeco Dai <zeeco.dai@think-bridge.com>
2020-03-11 15:21:39 +0800

    feat: add question 215

--- 215.数组中的第K个最大元素.js
+++ 215.数组中的第K个最大元素.js
@@ -8,14 +8,14 @@
 在未排序的数组中找到第 k 个最大的元素。请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
 输入：[3,2,1,5,6,4] 和 k = 2
 输出: 5
+
+选择不是很会到时候再补充。。
 */
 var findKthLargest = function(nums, k) {
     nums = quickSort(nums)
     return nums[nums.length - k]
 }   
-//选择排序
-
-//快排
+//快排，
 const quickSort = function(arr) {
     // 取出中间数，然后将数组遍历一遍  与 中间数坐比较 小的放左边，大的放右边
     if(arr.length <= 1) return arr
